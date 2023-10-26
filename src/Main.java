9f617d35c36ddd19c65ed8ff807102a65e2cad87
Cecilia Ringkvist <ceciliaringkvist@Cecilias-MacBook-Pro-2.local>
2023-10-26 12:42:37 +0200

    välkommen

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Game game;
+        Player player = new Player();
         do {
             Board gameBoard = new Board();
             game = new Game(gameBoard);
-            System.out.println("Welcome to Minesweeper!");
+            System.out.println("Welcome " + player.getName() + ", to Minesweeper!");
             game.start();
         } while (game.playAgain());
 
