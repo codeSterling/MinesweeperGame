cb4d784d5c4fdd807849955bf66b679905cff315
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-31 11:48:34 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
             } while (!scanner.hasNextInt());
 
             int boardSize = scanner.nextInt();
+            int boardSize = scanner.nextInt();
             game = new Game(boardSize, player);
 
             System.out.println("Välj svårighetsgrad (1-3):");
