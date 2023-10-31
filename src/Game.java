3fd884f1ca888a8196e027b32288f62186f12ab2
Maida <maidasahiti@MacBook-Air-som-tillhor-Maida.local>
2023-10-31 11:32:07 +0100

    Felhantering

--- src/Game.java
+++ src/Game.java
@@ -33,66 +33,56 @@ public class Game {
         public int playGame() {
         while (!gameOver) {
             gameBoard.printBoard();
-            System.out.print("Enter row and column (e.g., 1 2): ");
-
+            System.out.print("Please enter row and column (e.g., a 2): ");
 
             try {
-                int row = scanner.nextInt();
-
+                String rowInput = scanner.next();
                 int col = scanner.nextInt();
-                scanner.nextLine();
-                //Ifall man träffar mina
-                if (row >= 1 && row <= gameBoard.getBoardSize()
-                        && col >= 1 && col <= gameBoard.getBoardSize()) {
-                    row--;
-                    col--;
-                    if (gameBoard.getGameBoardElement(row, col) == '*') {
-                        gameBoard.revealCell(row, col);
-                        gameOver = true;
-
-                        System.out.println("Game over! You hit a mine.");
-                        gameBoard.showBoardWhenLooses();
-                    } else {
-                        // Visar med revealCell-metoden position med X
-                        gameBoard.revealCell(row, col);
-                        //Räknar antalet X för vinst
-                        int nonMineCells = (gameBoard.getBoardSize() * gameBoard.getBoardSize())
-                                - gameBoard.getNumberOfMines();
-                        int markedCells = 0;
-
-                        if (gameBoard.numberOfSquaresRevealed() == nonMineCells) {
-
-                            gameOver = true;
-                            System.out.println("Congratulations! You win!\uD83C\uDF89\uD83C\uDF89");
-                            player.incrementWins();
-                            System.out.println("You have won " + player.getWins() + " times!");
-                        }
-
-
-                    }
-
+                // scanner.nextLine();
+                int row = rowInput.toLowerCase().charAt(0) - 96;
+                if (rowInput.matches(".*\\d.*")) {      // a regex looks if the string contains a number
+                    System.out.println("Invalid input. That's a number. Please enter a letter as row!");
+                } else if (rowInput.length() > 1) {
+                    System.out.println("Invalid input. You have written more than a letter. Try again!");
                 } else {
+                    //Ifall man träffar mina
+                    if (row >= 1 && row <= gameBoard.getBoardSize()
+                            && col >= 1 && col <= gameBoard.getBoardSize()) {
+                        row--;
+                        col--;
+                        if (gameBoard.getGameBoardElement(row, col) == '*') {
+                            gameBoard.revealCell(row, col);
+                            gameOver = true;
+
+                            System.out.println("Game over! You hit a mine.");
+                            gameBoard.showBoardWhenLooses();
+                        } else {
+                            // Visar med revealCell-metoden position med X
+                            gameBoard.revealCell(row, col);
+                            //Räknar antalet X för vinst
+                            int nonMineCells = (gameBoard.getBoardSize() * gameBoard.getBoardSize())
+                                    - gameBoard.getNumberOfMines();
+
+                            if (gameBoard.numberOfSquaresRevealed() == nonMineCells) {
+
+                                gameOver = true;
+                                System.out.println("Congratulations! You win!\uD83C\uDF89\uD83C\uDF89");
+                                player.incrementWins();
+                                System.out.println("You have won " + player.getWins() + " times!");
+                            }
+                        }
+                    }
+                }
+            } catch (java.util.InputMismatchException e){
                     System.out.println("Invalid input. " +
                             "Please enter row and column within the valid range.");
-
-                }
-
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Invalid input. " +
-                        "Please enter row and column as integers, e.g., 1 2.");
-                scanner.nextLine();
-
+                    scanner.nextLine();
             }
-
         }
 
         gameBoard.printBoard();
         System.out.println("Thank you for playing!");
 
-        return 0;
-    }
-
-    private int setDifficulty(int difficulty) {
         return 0;
     }
 
