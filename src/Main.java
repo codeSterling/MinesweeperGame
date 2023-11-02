10df0ef2e680dc0be243fa5916765ccb1e093c9f
Jonas Bondesson <jonasbondesson@yahoo.com>
2023-11-02 09:39:49 +0100

    removed unused code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println();
         System.out.println("Welcome to \uD83D\uDCA5 Minesweeper \uD83D\uDCA5");
         System.out.println();
